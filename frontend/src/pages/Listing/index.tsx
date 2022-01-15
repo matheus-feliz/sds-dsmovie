8adfb096cd294db7c4ce81c7b8a64cebc4f3d835
sem-nome <matheusfoliveira172@gmail.com>
2022-01-15 20:58:48 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,41 +1,55 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "type/movie";
 import { BASE_URL } from "utils/requests";
 
 
 function listing() {
-//forma errada
 
-    axios.get(`${BASE_URL}movies?size=12&page=0`)
-    .then(response => {
-        console.log(response.data)
-    });
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
+            });
+
+    }, []);
+
+
+    //forma errada
+
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
                 <div className="row">
-                    
+
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
 
-                      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
 
-                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
 
-                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
 
-                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
 
